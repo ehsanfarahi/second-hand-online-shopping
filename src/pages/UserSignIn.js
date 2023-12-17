a6e95a4d87fa22940ebdceadc383f1637c498a05
Ehsan Farahi <ehsanullah.farahi12@gmail.com>
2023-12-18 00:44:45 +0100

    sign in form transition

--- src/pages/UserSignIn.js
+++ src/pages/UserSignIn.js
@@ -81,9 +81,9 @@ const UserSignIn = () => {
             <h3
               className={`${
                 mobileSigninupForm
-                  ? "text-2xl text-center uppercase transition-all duration-700 ease-in-out"
-                  : "text-lg rotate-[90deg] whitespace-nowrap flex justify-center items-center h-full font-semibold"
-              } `}
+                  ? "text-2xl text-center uppercase"
+                  : "text-lg rotate-[90deg] whitespace-nowrap flex justify-center items-center h-full font-semibold transition-all duration-700 ease-in-out"
+              } w-fit mx-auto transition-all duration-700 ease-in-out`}
             >
               Sign in
             </h3>
