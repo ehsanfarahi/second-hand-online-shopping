c61bb76fdeb7f0b9bb776ecd0386367fc8bf0340
Ehsan Farahi <ehsanullah.farahi12@gmail.com>
2024-06-07 02:53:29 +0200

    further update

--- json-server/api/[...path].js
+++ json-server/api/[...path].js
@@ -3,7 +3,17 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+server.use((req, res, next) => {
+  console.log(`Received request: ${req.method} ${req.url}`);
+  next();
+});
+
 server.use(middlewares);
 server.use(router);
+
+server.use((err, req, res, next) => {
+  console.error('Server error:', err);
+  res.status(500).send('Internal Server Error');
+});
 
 module.exports = server;
